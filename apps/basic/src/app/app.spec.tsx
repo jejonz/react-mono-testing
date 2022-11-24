f67b9ce21c87ccaf0e1f83516bf42eaa508fa112
Johan Johansson <80477851+jejonz@users.noreply.github.com>
2022-11-24 15:19:14 +0100

    Added basic app with tests

--- apps/basic/src/app/app.spec.tsx
+++ apps/basic/src/app/app.spec.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import App from './app';
 
@@ -10,8 +10,42 @@ describe('App', () => {
   });
 
   it('should have a greeting as the title', () => {
-    const { getByText } = render(<App />);
+    const { getByText, getByTestId } = render(<App />);
 
-    expect(getByText(/Welcome basic/gi)).toBeTruthy();
+    expect(getByText(/Welcome Basic/)).toBeTruthy();
+    expect(getByTestId('title').textContent).toEqual('Welcome Basic!');
+  });
+
+  it('should have a static label with text', () => {
+    const { getByText, getByTestId } = render(<App />);
+
+    expect(getByText(/Data Input:/)).toBeTruthy();
+    expect(getByTestId('label').textContent).toEqual('Data Input:');
+  });
+
+  it('should have an input without text initially', () => {
+    const { getByTestId } = render(<App />);
+
+    const inputField = getByTestId('input') as HTMLInputElement;
+    expect(inputField.value).toEqual('');
+  });
+
+  it('should be possible to enter text', () => {
+    const { getByTestId } = render(<App />);
+
+    let inputField = getByTestId('input') as HTMLInputElement;
+    fireEvent.change(inputField, {
+      target: {
+        value: 'text 1',
+      },
+    });
+
+    // alternative way:
+    inputField = getByTestId('input') as HTMLInputElement;
+    inputField.value += ', text 2';
+    inputField.dispatchEvent(new Event('input'));
+
+    inputField = getByTestId('input') as HTMLInputElement;
+    expect(inputField.value).toEqual('text 1, text 2');
   });
 });
--- apps/basic/src/app/app.tsx
+++ apps/basic/src/app/app.tsx
@@ -1,13 +1,31 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { useState } from 'react';
 import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
 
 export function App() {
+  const [inputData, setInputData] = useState('');
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputData(event.target.value);
+  };
+
   return (
-    <>
-      <NxWelcome title="basic" />
-      <div />
-    </>
+    <div>
+      <h1 data-testid="title" className={styles.h1}>
+        Welcome Basic!
+      </h1>
+      <div className={styles.inputContainer}>
+        <label data-testid="label" className={styles.label}>
+          Data Input:
+        </label>
+        <input
+          data-testid="input"
+          className={styles.input}
+          value={inputData}
+          onChange={onChange}
+        />
+      </div>
+    </div>
   );
 }
 
